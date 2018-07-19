18741541f7c226e980afc0ade0902a606deb850b
姚晨嵩 <yaochensong@meituan.com>
2018-07-19 09:04:47 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vam-m3u8-downloader'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
